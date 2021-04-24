496dee0c417773e58a4d97f415a1f6e11ef0d1a9
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-04-25 06:45:54 +0700

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 4000
 
 app.use(express.json())
-
+app.use(cors())
 const routes = require("./src/routes/index")
 
 // Connect Database
