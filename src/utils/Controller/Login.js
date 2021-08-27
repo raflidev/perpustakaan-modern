b1e5a6a8b20daf57c5698c32d6ca6833d87ce176
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-08-27 17:30:42 +0700

    jwt expired token implemetation

--- src/utils/Controller/Login.js
+++ src/utils/Controller/Login.js
@@ -5,7 +5,18 @@ require("dotenv/config")
 exports.tryLogin = (req,res) => {
     User.find({email: req.body.email, password: req.body.password}).then(user => {
         res.status(200).json({
-            "token": jwt.sign(JSON.stringify(user), process.env.JWT_SECRET),
+            "token": jwt.sign({
+                "role":user[0].role,
+                "_id":user[0]._id,
+                "username":user[0].username,
+                "full_name":user[0].full_name,
+                "email":user[0].email,
+                "valid":user[0].valid,
+                "password":user[0].password,
+                "__v":user[0].__v,
+            }, process.env.JWT_SECRET, {
+                expiresIn: "4h"
+            }),
             "message": 'berhasil login'
         })
     }).catch(err => {
@@ -14,8 +25,14 @@ exports.tryLogin = (req,res) => {
 }
 
 exports.verifyToken = (req,res) => {
-    res.status(200).json({
-        "user": jwt.verify(req.body.token, process.env.JWT_SECRET),
-        "message": "token berhasil divefikasi"
-    })
+    try {
+        res.status(200).json({
+            "user": jwt.verify(req.body.token, process.env.JWT_SECRET),
+            "message": "token berhasil divefikasi"
+        })   
+    } catch (error) {
+        res.status(500).json({
+            "message": "token expired"
+        })
+    }
 }
